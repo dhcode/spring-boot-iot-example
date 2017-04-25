09cee388b203dc7b218fc96aafcf724ef91ed17f
DominikH <dominik@my1a.de>
2017-04-25 21:45:03 +0200

    Fix annotations

--- src/main/java/com/example/iot/data/repository/SensorRepository.java
+++ src/main/java/com/example/iot/data/repository/SensorRepository.java
@@ -5,7 +5,8 @@ package com.example.iot.data.repository;
 
 import com.example.iot.data.entity.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface SensorRepository extends JpaRepository<Sensor, Long> {
 }
